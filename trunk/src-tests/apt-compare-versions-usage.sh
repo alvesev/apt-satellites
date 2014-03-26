222859c0e5ed4ecf8ee4ee9c5466fee57cdc9834
Alex Vesev <alex.vesev@gmail.com>
2014-03-26 16:19:48 +0400

    Initial release.

---
+++ trunk/src-tests/apt-compare-versions-usage.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+#  Copyright 2013 Alex Vesev
+#
+#  This file is part of Apt Satellites.
+#
+#  Apt Satellites is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Apt Satellites is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Apt Satellites.  If not, see <http://www.gnu.org/licenses/>.
+#
+##
+
+
+PS4="CMD:${0}:pid=\${$}: "
+set -x
+
+../src/apt-compare-versions --left "9.0.7-1~bpo70" --right "9.0.7-1~bpo70+1"
+echo "    Exit value : ${?}"
+
+../src/apt-compare-versions -l "9.0.7-1" -r "9.0.7-1"
+echo "    Exit value : ${?}"
+
+../src/apt-compare-versions -l "9.0.7-1~bpo70+1" -r "9.0.7-1~bpo70"
+echo "    Exit value : ${?}"
